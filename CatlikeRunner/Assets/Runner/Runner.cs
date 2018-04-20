c6520a0c02492e461cc6cdc4c8ce50e2767cce90
zuluonezero <38572363+zuluonezero@users.noreply.github.com>
2018-04-21 07:43:03 +1000

    Initial Project upload
    Library folder removed

---
+++ CatlikeRunner/Assets/Runner/Runner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner : MonoBehaviour {
+
+    public static float distanceTraveled;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(5f * Time.deltaTime, 0f, 0f);
+        distanceTraveled = transform.localPosition.x;
+    }
+}
---
+++ CatlikeRunner/Assets/SkyLine/SkyLineManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyLineManager : MonoBehaviour
+{
+
+    public Transform prefab;
+    public int numberOfObjects;
+    public float recycleOffset;
+    public Vector3 startPosition;
+ 
+
+    private Vector3 nextPosition;
+    private Queue<Transform> objectQueue;
+
+    // Use this for initialization
+    void Start()
+    {
+        objectQueue = new Queue<Transform>(numberOfObjects);
+        nextPosition = startPosition;
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            Transform o = (Transform)Instantiate(prefab);
+            o.localPosition = nextPosition;
+            nextPosition.x += o.localScale.x;
+            objectQueue.Enqueue(o);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (objectQueue.Peek().localPosition.x + recycleOffset < Runner.distanceTraveled)
+        {
+            Transform o = objectQueue.Dequeue();
+            o.localPosition = nextPosition;
+            nextPosition.x += o.localScale.x;
+            objectQueue.Enqueue(o);
+        }
+    }
+}
